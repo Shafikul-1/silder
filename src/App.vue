4902db596f225c3b8fdd14287a06b7a1577e5217
Shafikul <shafikul.18288@gmail.com>
2023-08-13 07:45:34 +0600

    carosul create

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+ import hellow from './components/HelloWorld.vue'
+ import carosul from './components/Carosul.vue'
+</script>
+
+<template>
+
+  <!-- <carosul/>  -->
+  <hellow/>
+</template>
+
+<style scoped>
+
+</style>
