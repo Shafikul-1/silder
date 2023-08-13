f0aa274cb817a7bad6df1c6d18f9f2ad2af2fb83
Shafikul <shafikul.18288@gmail.com>
2023-08-13 22:48:39 +0600

    some modification

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@ import PiaChart from './components/PiaChart.vue';
 </script>
 
 <template>
-
+  <h1 class="my-5 font-bold text-3xl text-center ">Carosul</h1>
   <carosul /> 
 
+  <h1 class="my-5 font-bold text-3xl text-center ">Pie Chart</h1>
   <PiaChart class="my-5"/>
 </template>
 
